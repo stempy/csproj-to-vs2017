49c234e7451084e7027e5be6e5865d1dbb230b3c
Robin Stemp <robs@iaanyware.net>
2017-03-29 15:01:21 +1000

    build self contained dotnet core --> windows exe see: https://docs.microsoft.com/en-us/dotnet/articles/core/deploying/

---
+++ src/CsProjToVs2017Upgrader/BuildExe.cmd
@@ -0,0 +1,9 @@
+@echo off
+set thisDir=%~dp0
+set thisDir=%thisDir:~0,-1%
+rem publish to local self contained exe file 
+
+pushd "%thisDir%"
+dotnet restore
+dotnet publish -c Release -r win10-x64 -o bin\Release\publishoutput
+popd
