91fb95216ec7ea89d2cb60e56cb137459d2ddace
Robin Stemp <robs@iaanyware.net>
2017-03-31 14:17:04 +1000

    generate on -g|--generate switch

--- src/CsProjToVs2017Upgrader/Program.cs
+++ src/CsProjToVs2017Upgrader/Program.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using CsProjToVs2017Upgrader.Models;
 
 namespace CsProjToVs2017Upgrader
 {
@@ -16,44 +17,25 @@ namespace CsProjToVs2017Upgrader
                 return;
             }
 
+            bool generateUpgrades = false;
+            if (args.Contains("--generate") || args.Contains("-g"))
+            {
+                generateUpgrades = true;
+                args = args.Where(m => m != "-g").ToArray();
+                args = args.Where(m => m != "--generate").ToArray();
+            }
+
             var ap = new ProjectAnalyzer();
-            var projectUpgrader = new UpgradeProjectToVs2017();
-            var slns = new Dictionary<string, string>();
 
             foreach (var arg in args)
             {
+                // 1. analyze projects
                 var projectsAnalyzed = ap.AnalyzeProjectsPath(arg);
 
-                bool copySolution = false;
-                string newSlnPath = string.Empty;
-                string slnSrc = string.Empty;
-
-                // now upgrade .csproj where suited
-                foreach (var project in projectsAnalyzed)
+                if (generateUpgrades)
                 {
-                    var newProjFile=projectUpgrader.UpgradeProject(project.ProjectFilePath);
-                    if (!string.IsNullOrEmpty(project.BelongsToSolutionFile) 
-                        &&  !copySolution)
-                    {
-                        slnSrc = project.BelongsToSolutionFile;
-                        var newSlnFileName = Path.GetFileNameWithoutExtension(project.BelongsToSolutionFile) + "_upgraded.sln";
-                        var slnDir = Path.GetDirectoryName(Path.GetDirectoryName(newProjFile));
-                        newSlnPath = Path.Combine(slnDir, newSlnFileName);
-                        copySolution = true;
-                    }
+                    GenerateUpgrades(projectsAnalyzed);
                 }
-
-                if (!string.IsNullOrEmpty(slnSrc))
-                {
-                    // has a sln 
-                    var slnText = File.ReadAllText(slnSrc);
-                    //slnText = slnText.Replace(".csproj", "_upgraded.csproj");
-                    File.WriteAllText(newSlnPath, slnText);
-                    Console.WriteLine("new sln " + newSlnPath);
-                }
-
-
-
             }
 
             if (Debugger.IsAttached)
@@ -66,9 +48,42 @@ namespace CsProjToVs2017Upgrader
         static void Usage()
         {
             Console.WriteLine("Old Csproj to VS2017 Upgrader\n=============================\nUsage:");
-            Console.WriteLine("\tCsProjToVs2017Upgrader slnfile1.sln slnfile2.sln projectfile.csproj projectfile2.csproj");
+            Console.WriteLine("\tCsProjToVs2017Upgrader [-g|--generate] slnfile1.sln slnfile2.sln projectfile.csproj projectfile2.csproj");
         }
 
+        static void GenerateUpgrades(IEnumerable<ProjectMeta> projects)
+        {
+            // upgrade if --generate | -g
+            bool copySolution = false;
+            string newSlnPath = string.Empty;
+            string slnSrc = string.Empty;
+
+            var projectUpgrader = new UpgradeProjectToVs2017();
+
+            // now upgrade .csproj where suited
+            foreach (var project in projects)
+            {
+                var newProjFile = projectUpgrader.UpgradeProject(project.ProjectFilePath);
+                if (!string.IsNullOrEmpty(project.BelongsToSolutionFile)
+                    && !copySolution)
+                {
+                    slnSrc = project.BelongsToSolutionFile;
+                    var newSlnFileName = Path.GetFileNameWithoutExtension(project.BelongsToSolutionFile) + "_upgraded.sln";
+                    var slnDir = Path.GetDirectoryName(Path.GetDirectoryName(newProjFile));
+                    newSlnPath = Path.Combine(slnDir, newSlnFileName);
+                    copySolution = true;
+                }
+            }
+
+            if (!string.IsNullOrEmpty(slnSrc))
+            {
+                // has a sln 
+                var slnText = File.ReadAllText(slnSrc);
+                //slnText = slnText.Replace(".csproj", "_upgraded.csproj");
+                File.WriteAllText(newSlnPath, slnText);
+                Console.WriteLine("new sln " + newSlnPath);
+            }
+        }
       
     }
 }
