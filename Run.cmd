c80d00702ceb04ae5739d9d935aa5bb1acf60cfc
Robin Stemp <robs@iaanyware.net>
2017-04-28 14:01:46 +1000

    fix up run batch to run from another directory properly

--- Run.cmd
+++ Run.cmd
@@ -1,9 +1,9 @@
 @echo off
+SETLOCAL
 set thisDir=%~dp0
 set thisDir=%thisDir:~0,-1%
+set outputPath=%thisDir%\src\CsProjToVs2017Upgrader\bin\release\publishoutput\CsProjToVs2017Upgrader.exe
 rem Run (build exe if needed)
-if not exist "%thisDir%\src\CsProjToVs2017Upgrader\bin\release\publishoutput\CsProjToVs2017Upgrader.exe" call "%thisDir%\BuildExe.cmd"
-
-pushd "%thisDir%\src\CsProjToVs2017Upgrader\bin\release\publishoutput"
-CsProjToVs2017Upgrader.exe %*
-popd
+if not exist "%outputPath%" call "%thisDir%\BuildExe.cmd"
+"%outputPath%" %*
+ENDLOCAL
