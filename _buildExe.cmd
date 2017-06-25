b04df288c47a1520ad8d9c03fe34f800fc1a38a6
Rob Stemp <robs@iaanyware.net>
2017-06-25 14:45:54 +1000

    build nuget packages for upgrader and nuget version app

--- _buildExe.cmd
+++ _buildExe.cmd
@@ -5,12 +5,12 @@ set thisDir=%~dp0
 set thisDir=%thisDir:~0,-1%
 set projDir=%~1
 set projName=%2
-call "%thisDir%\CleanBinObj.cmd"
+rem call "%thisDir%\CleanBinObj.cmd"
 call "%thisDir%\_setPublishOutput.cmd" %projName%
 if exist "%publishOutputDir%" rd /q/s "%publishOutputDir%"
 rem ------------ now do it
 pushd "%projDir%"
-dotnet restore
-dotnet publish -c Release -r win10-x64 -o %publishOutputDir%
+call dotnet restore
+call dotnet publish -c Release -r win10-x64 -o %publishOutputDir%
 popd
 ENDLOCAL
