27f88143456657fa7b2705181f11a9ab7661ef88
Robin Stemp <rob@stempnet.com>
2017-07-28 14:37:18 +1000

    build nuget packages

--- BuildNugetPackages.cmd
+++ BuildNugetPackages.cmd
@@ -3,18 +3,33 @@ SETLOCAL
 set thisDir=%~dp0
 set thisDir=%thisDir:~0,-1%
 set buildDir=%thisDir%\Build
+set pkgBin=%thisDir%\bin
 if NOT DEFINED PackageVersion set PackageVersion=1.0.5
 if NOT exist "%NuGet%" echo Nuget var not set & exit /b 1
 if NOT exist "%buildDir%" md "%buildDir%"
-
+if NOT exist "%pkgBin%" md "%pkgBin%"
+if NOT DEFINED GitVersion set GitVersion=%pkgBin%\GitVersion.CommandLine.3.6.5\tools\gitversion.exe
 if "%config%"=="" set config=Release
+
+if not exist "%pkgBin%\gitversion.commandline.*" call :installgitversion
+if not exist "%GitVersion%" echo unable to find gitversion & exit /b 1
+
+pushd "%thisDir%"
+for /f %%a in ('gitversion /showvariable NuGetVersionV2') do set PackageVersion=%%a
+
 set version=
 
 rem build .net core packages
 call :buildAndPack "%thisDir%\src\CsProjToVs2017Upgrader"
 call :buildAndPack "%thisDir%\src\NugetVersion"
+
+popd
 exit /b
 
+
+:installgitversion
+call "%NuGet%" install GitVersion.CommandLine -Version 3.6.5 -o "%pkgBin%"
+exit /b %errorlevel%
 
 :buildAndPack
 set pDir=%~1
